4752cf9279c851b75b96b83443d77d74111449e0
nandafachini <maria22296@hotmail.com>
2022-10-06 13:40:44 -0300

    programa principal

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// #include "metro.hpp"
+#include "socioMetro.hpp"
+#include "gerenteMetro.hpp"
+#include "donoMetro.hpp"
+
+
+int main() {
+
+    socioMetro();
+
+    gerenteMetro();
+
+    donoMetro();
+
+    return 0;
+
+}
